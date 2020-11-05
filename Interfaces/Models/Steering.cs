fb62464e938b0763c6aaafdbd309ef5ddf49d20f
Cowportal <73531403+Cowportal@users.noreply.github.com>
2020-11-05 17:08:50 +0200

    Add project files.

---
+++ Interfaces/Models/Steering.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Models
+{
+    class Steering
+    {
+		private bool _steering;
+
+        public bool GetSteering()
+        { return _steering; }
+        private protected void SetSteering(bool value)
+        { _steering = value; }
+        public Steering()
+		{
+
+		}
+
+		public Steering (bool IsOn)
+		{
+			this._steering = IsOn;
+		}
+
+		public void SteeringL()
+		{
+			this._steering = true;
+		}
+
+		public void SteeringR()
+		{
+			this._steering = false;
+		}
+	}
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,51 @@
+﻿using Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("car: c");
+            Car c = new Car();
+            Console.WriteLine(c.IsOn);
+            c.Start();
+            c.SteerLeft();
+            c.SteerRight();
+            c.Reverse();
+            c.Break();
+            c.Stop();
+            Console.WriteLine(c.IsOn);
+
+            Console.WriteLine("motorbike: m");
+            MotorBike m = new MotorBike();
+            Console.WriteLine(m.IsOn);
+            m.Start();
+            m.SteerLeft();
+            m.SteerRight();
+            m.Break();
+            m.Stop();
+            Console.WriteLine(m.IsOn);
+
+            Console.WriteLine("boat: b");
+            Boat b = new Boat();
+            Console.WriteLine(b.IsOn);
+            b.Start();
+            b.SteerLeft();
+            b.SteerRight();
+            b.Reverse();
+            b.Stop();
+            Console.WriteLine(b.IsOn);
+
+
+
+
+        }
+    }
+}
